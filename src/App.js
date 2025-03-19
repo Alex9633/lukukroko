62c737edc3c05981a08dbf2ee0c062b3c24506d6
Alex9633 <120327803+Alex9633@users.noreply.github.com>
2025-03-19 18:16:21 +0100

    files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="title">TULOSSA PIAN!</div>
+      <img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="logo" className="logo" />
+      
+      <div className="content">
+        <div className="subtitle">KROKON SUURI HYMY</div>
+        <p>
+        Syvällä viidakossa, kirkkaan sinisen joen rannalla, asui yksinäinen krokotiili nimeltä Kroko. Hän rakasti leikkiä ja seikkailla, mutta kukaan ei halunnut leikkiä hänen kanssaan.  <br></br>
+        Aina kun Kroko loiskautti vettä, apinat kiipesivät pelokkaasti korkeimmille oksille. Kun hän asteli ruohikossa, peurat pinkaisivat pakoon. Ja aina, kun hän hymyili linnuille, ne lehahtivat kauemmas.  <br></br>
+        "Miksi kukaan ei halua olla ystäväni?" Kroko huokaisi surullisena.  <br></br>
+        Eräänä päivänä pieni sudenkorento nimeltä Teemu pörähti Krokon kuonolle. "Miksi näytät niin surulliselta?" hän kysyi huolestuneena.  <br></br>
+        "Kukaan ei halua leikkiä kanssani", Kroko vastasi. "Ne luulevat, että syön ne!"  <br></br>
+        Teemu mietti hetken ja surisi sitten: "Ehkä se johtuu hampaistasi! Kun hymyilet, ne näyttävät todella teräviltä. Mutta hymy ei ole pelkästään hampaita. Harjoitellaanpa ystävällisempää hymyä!"  <br></br>
+        Kroko katseli heijastustaan vedessä. Teemu näytti hänelle, kuinka silmiä voi pehmentää ja päätä kallistaa hieman. "Noin! Ei lainkaan pelottava!" Teemu hihkaisi iloisesti.  <br></br>
+        "Mutta miten muut voivat tietää, etten ole vaarallinen?" Kroko kysyi epäilevästi.  <br></br>
+        Teemulla oli suunnitelma. "Näytetään heille!"  <br></br>
+        Teemu lensi apinoiden luo ja surisi: "Kroko haluaa auttaa teitä saamaan hedelmiä!" Apinat epäröivät hetken, mutta kun he näkivät Krokon lempeästi heilauttavan häntäänsä ja ravistavan puuta, ne alkoivat iloisesti poimia putoilevia hedelmiä.  <br></br>
+        Seuraavaksi Teemu lenteli lintujen luo ja sanoi: "Tulkaa katsomaan! Kroko tekee veteen hauskoja renkaita, joilla voitte leikkiä!" Linnut uskaltautuivat lähemmäs ja visersivät riemusta loikkiessaan aalloilla.  <br></br>
+        Lopuksi Teemu vei peurat joen reunalle ja sanoi: "Kroko pitää teidät turvassa, kun juotte." Peurat astelivat varovasti lähemmäs. Kroko pysyi täysin paikallaan ja hymyili varovasti. Hetken kuluttua peurat ymmärsivät, ettei hän ollutkaan vaarallinen.  <br></br>
+        Yksi toisensa jälkeen eläimet huomasivat, että Kroko ei ollut pelottava—hän oli kiltti ja hauska!  Siitä päivästä lähtien Kroko ei ollut enää koskaan yksinäinen. Hänen suuri hymynsä ei enää pelottanut ketään—se toi kaikki yhteen.
+        </p>
+      </div>
+      <br></br>
+      <br></br>
+      <footer>Copyright © 2025 LUKUKROKO</footer>
+    </div>
+  );
+}
+
+export default App;
